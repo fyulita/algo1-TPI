d9569f2d0ba8dc05d132bba533ccb479f72791a2
unknown <kenanchab@gmail.com>
2019-10-22 10:45:41 -0300

    ej2 testeado

--- auxiliares.cpp
+++ auxiliares.cpp
@@ -42,8 +42,112 @@ bool esBinaria(const imagen &img) {
 
 /* Segundo Ejercicio */
 
+bool pixelValidoEncendido(pixel p, imagen A){
+    if(p.size() != 2){
+        return false;
+    }
+    if(p[0] < A.size() && p[0] >= 0 && p[1] >= 0 && p[1] < A[0].size() && A[p[0]][p[1]] == 1){
+        return true;
+    }
+    return false;
+}
+
+bool perteneceALista(pixel p, sqPixel sq){
+    int i= sq.size()-1;
+    bool resp = false;
+    while(i >= 0 ){
+        if (sq[i] == p)
+            resp = true;
+        i--;
+    }
+    return resp;
+}
+
 bool existeCamino(const imagen &A, const pixel &p, const pixel &q, int k) {
     bool resp;
+    int numPixelA = A.size() * A[0].size(), i = 0;
+    sqPixel figura(0) ;
+    figura.push_back(p);
+    sqPixel nuevaFigura;
+    nuevaFigura.push_back(p);
+    pixel tempArriba=p, tempAbajo=p, tempIzquierda=p, tempDerecha=p;
+    pixel tempAbajoIzquierda=p, tempAbajoDerecha=p, tempArribaIzquierda=p, tempArribaDerecha=p;
+    while(numPixelA > 0)
+    {
+        i = figura.size()-1;
+        while (figura == nuevaFigura && i >= 0 )
+        {
+            if(k==8){
+                tempArribaDerecha[1] = nuevaFigura[i][1] + 1;
+                tempArribaDerecha[0] =nuevaFigura[i][0] + 1;
+                if(not(perteneceALista(tempArribaDerecha, nuevaFigura)) && pixelValidoEncendido(tempArribaDerecha, A) == true){
+                    nuevaFigura.push_back(tempArribaDerecha);
+                }
+
+                tempArribaIzquierda[1] = nuevaFigura[i][1] + 1;
+                tempArribaIzquierda[0] =nuevaFigura[i][0] - 1;
+                if(not(perteneceALista(tempArribaIzquierda,nuevaFigura)) && pixelValidoEncendido(tempArribaIzquierda, A) == true)
+                {
+                    nuevaFigura.push_back(tempArribaIzquierda);
+                }
+
+                tempAbajoIzquierda[1] = nuevaFigura[i][1] - 1;
+                tempAbajoIzquierda[0] =nuevaFigura[i][0] - 1;
+                if(not(perteneceALista(tempAbajoIzquierda, nuevaFigura)) && pixelValidoEncendido(tempAbajoIzquierda, A) == true)
+                {
+                    nuevaFigura.push_back(tempAbajoIzquierda);
+                }
+
+                tempAbajoDerecha[1] = nuevaFigura[i][1] - 1;
+                tempAbajoDerecha[0] =nuevaFigura[i][0] + 1;
+                if(not(perteneceALista(tempAbajoDerecha, nuevaFigura)) && pixelValidoEncendido(tempAbajoDerecha, A) == true)
+                {
+                    nuevaFigura.push_back(tempAbajoDerecha);
+                }
+            }
+            tempArriba[0] = nuevaFigura[i][0];
+            tempArriba[1] = nuevaFigura[i][1] + 1;
+
+            tempAbajo[0] = nuevaFigura[i][0];
+            tempAbajo[1] = nuevaFigura[i][1] - 1;
+
+            tempIzquierda[1] = nuevaFigura[i][1];
+            tempIzquierda[0] = nuevaFigura[i][0] - 1;
+
+            tempDerecha[1] = nuevaFigura[i][1];
+            tempDerecha[0] = nuevaFigura[i][0] + 1;
+
+            if(not(perteneceALista(tempArriba, nuevaFigura)) && pixelValidoEncendido(tempArriba, A) == true)
+            {
+                nuevaFigura.push_back(tempArriba);
+            }
+
+            if(not(perteneceALista(tempAbajo, nuevaFigura)) && pixelValidoEncendido(tempAbajo, A) == true)
+            {
+                nuevaFigura.push_back(tempAbajo);
+            }
+
+            if(not(perteneceALista(tempDerecha, nuevaFigura)) && pixelValidoEncendido(tempDerecha, A) == true)
+            {
+                nuevaFigura.push_back(tempDerecha);
+            }
+
+            if(not(perteneceALista(tempIzquierda, nuevaFigura)) && pixelValidoEncendido(tempIzquierda, A) == true)
+            {
+                nuevaFigura.push_back(tempIzquierda);
+            }
+
+            i--;
+        }
+
+        figura = nuevaFigura;
+
+        numPixelA--;
+    }
+    if(perteneceALista(q,figura))
+    {
+        resp = true;
+    }
 
     return resp;
 }
